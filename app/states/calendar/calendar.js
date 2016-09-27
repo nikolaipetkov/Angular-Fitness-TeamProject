7352733b5181477275c275d3fc0d6208033f90a0
mihaildimitrov <mihail.ko.dimitrov@abv.bg>
2016-09-27 21:02:42 +0300

    Add calendar state

---
+++ app/states/calendar/calendar.js
@@ -0,0 +1,58 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('calendar', calendar)
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('calendar', {
+                url: '/calendar',
+                template: '<calendar></calendar>'
+            });
+    }
+
+    function calendar() {
+        var directive = {
+            templateUrl: './states/calendar/calendar.html',
+            restrict: 'E',
+            controller: controller,
+            scope: {
+                name: '='
+            }
+        };
+
+        return directive;
+    }
+
+    controller.$inject = ['$scope', 'survey', '$timeout'];
+    function controller($scope, survey, $timeout) {
+        $scope.survey = survey;
+
+        $scope.sayHi = sayHi;
+        $scope.sayHi1 = sayHi1;
+        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+        function sayHi() {
+            alert('Hi to you ' + $scope.name + '! Give me ' + survey.getTotalNumber() + '!');
+        }
+
+        function sayHi1() {
+            return 'Hi Maria!';
+        }
+
+        var map;
+          function initMap() {
+            console.log('qqq!');
+            map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: -34.397, lng: 150.644},
+              zoom: 8
+            });
+          }
+
+    }
+
+}(angular));
