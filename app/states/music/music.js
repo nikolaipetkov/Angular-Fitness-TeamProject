47723add93b9a3c188006499b34095d242b6ad9e
danzin <excl.d.k@gmail.com>
2016-09-28 15:03:48 +0300

    rough view of music player, changes incomming

--- app/states/music/music.js
+++ app/states/music/music.js
@@ -30,39 +30,61 @@
     }
 
     controller.$inject = ['$scope', 'survey', '$timeout'];
-    function controller($scope, survey, $timeout) {
-        
-        
-        $scope.songs = [
-        {
-            id: 0,
-            name: 'name',
-            author: 'some author',
-            duration: '3:20',
-            date: '01-02-2016',
-            url: 'some/url/with/songs'
-        }];
-        $scope.markAll = false;
+        function controller($scope, survey, $timeout) {
+            var id = 1;
+            $scope.songs = [
+            {
+                id: 0,
+                name: 'nameofsong1',
+                author: 'some author1',
+                duration: '3:20',
+                date: '01-02-2016',
+                url: 'some/url/with/songs'
+            },{
+                id: 1,
+                name: 'nameofsong1',
+                author: 'some author1',
+                duration: '3:20',
+                date: '01-02-2016',
+                url: 'some/url/with/songs'
+            }];
+            $scope.markAll = false;
 
-        function addSong() {
-            if(event.keyCode == 13 && $scope.songText){
-            $scope.songs.push({text:$scope.songText, done:false});
-            $scope.SongText = '';
-            }
-        }
-        function isSong() {
-                  return $scope.songs.length > 0;  
-        }
-        
-      function clear() {
+
+       $scope.addSong = function () {
+        id++;
+        $scope.songs.push(
+            {
+                id: id,
+                name:$scope.songName,
+                author:$scope.songAuthor,
+                duration:$scope.songDuration,
+                date:$scope.songDate,
+                done:false
+            });
+        $scope.songName = '';
+        $scope.songAuthor = '';
+        $scope.songDate = '';
+        $scope.songDuration = '';
+        };
+           
+        $scope.removeSong = function() {
         var oldSongs = $scope.songs;
-            $scope.songs = [];
-            angular.forEach(oldSongs, function(song) {
-          if (!todo.done) $scope.songs.push(song);
+        $scope.songs = [];
+        angular.forEach(oldSongs, function(song) {
+            if (!song.done) {$scope.songs.push(song);}
         });
-      }
+        };    
 
-    
+        $scope.toggleMarkAll = function () {
+            var songs = $scope.songs;
+            angular.forEach(songs, function (song) {
+                    song.done = true;
+                    id--;
+            });
+        };
 
+
+    }
 }
-}(angular));
+(angular));
