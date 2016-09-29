32c4d59009276cd7e7cfc5750c64c0a6cbadca43
Nikolai <Nikolai>
2016-09-29 17:24:03 +0300

    updatedHomepageLayout

--- app/states/niki/nikiStateJS.js
+++ app/states/niki/nikiStateJS.js
@@ -29,32 +29,22 @@
         return directive;
     }
 
-    controller.$inject = ['$scope', 'survey', '$timeout'];
-    function controller($scope, survey, $timeout) {
+    controller.$inject = ['$scope', 'survey'];
+    function controller($scope, survey) {
         $scope.survey = survey;
 
-        $scope.sayHi = sayHi;
-        $scope.sayHi1 = sayHi1;
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+        $scope.setBackground = setBackground;
+        $scope.test1 = test1;
 
-        function sayHi() {
-            alert('Hi to you ' + $scope.name + '! Give me ' + survey.getTotalNumber() + '!');
+//Ask Todor about this !!!
+        function setBackground() {
+            $scope.background = "url(/states/niki/bg1.jpg";
         }
 
-        function sayHi1() {
-            return 'Hi Maria!';
+        function test1() {
+            return 'Test Success';
         }
 
-        var map;
-          function initMap() {
-            console.log('qqq!');
-            map = new google.maps.Map(document.getElementById('map'), {
-              center: {lat: -34.397, lng: 150.644},
-              zoom: 8
-            });
-          }
-
-        $timeout(initMap, 2000);
 
     }
 
