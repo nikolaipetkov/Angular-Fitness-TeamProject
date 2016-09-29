faada9702c59d1c313887f5b21222fb264c9ba27
Tsvetelina Baycheva <tbayctc@training.local>
2016-09-29 21:14:26 +0300

    Adding new file

--- app/states/dictionary/dictionary.js
+++ app/states/dictionary/dictionary.js
@@ -21,41 +21,53 @@
             templateUrl: './states/dictionary/dictionary.html',
             restrict: 'E',
             controller: controller,
-            scope: {
-                name: '='
-            }
+            scope: {}
         };
 
         return directive;
     }
 
-    controller.$inject = ['$scope', 'survey', '$timeout'];
-    function controller($scope, survey, $timeout) {
-        $scope.survey = survey;
+    controller.$inject = ['$scope'];
+    function controller($scope) {
+        $scope.table =[
+        {
+            'name' : 'kljenj',
+            'description' : 'kvbejiobneriovbrnvbier'
+        },
+        {
+            'name' : 'njedogjer',
+            'description': 'klnbbbolmoebenrobvrnme'
+        },
+        {
+            'name' : 'nejvne',
+            'description': 'lkvnmoblerkn'
+        },
+        ]; 
 
-        $scope.sayHi = sayHi;
-        $scope.sayHi1 = sayHi1;
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-
-        function sayHi() {
-            alert('Hi to you ' + $scope.name + '! Give me ' + survey.getTotalNumber() + '!');
+        $scope.addRow = function() {
+            $scope.table.push({'name':$scope.name, 'description' : $scope.description});
+            $scope.name = '';
+            $scope.description = '';
         }
 
-        function sayHi1() {
-            return 'Hi Tsvetelina!';
+        $scope.editExercises = function(x) {
+            $scope.current = angular.copy(x);
+            x.editMode = true;
+        };
+
+         $scope.saveChanges = function(x) {
+                x.editMode = false;
+            };
+
+        $scope.removeExercise = function(index) {
+            $scope.table.splice(index, 1);
+        };
+
+        $scope.cancel = function(x) {
+            x.editMode = false;
+            x.name = $scope.current.name;
+            x.description = $scope.current.name;
         }
-
-        var map;
-          function initMap() {
-            console.log('qqq!');
-            map = new google.maps.Map(document.getElementById('map'), {
-              center: {lat: -34.397, lng: 150.644},
-              zoom: 8
-            });
-          }
-
-        $timeout(initMap, 2000);
-
     }
 
 }(angular));
