1ab6d10d8306648d4d65c9b5ddd2aa17aeb943e4
Nofirt <trifon.j.trifonov@gmail.com>
2016-10-03 22:13:47 +0300

    Add working calculator

---
+++ app/states/calculator/calculator.js
@@ -0,0 +1,42 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('calculator', calculator)
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('Calculator', {
+                url: '/calculator',
+                template: '<calculator></calculator>',
+            });
+    }
+
+    function calculator() {
+        var directive = {
+            templateUrl: './states/calculator/calculator.html',
+            restrict: 'E',
+            controller: controller,
+            scope: {}
+        };
+        return directive;
+    }
+
+    controller.$inject = ['$scope', 'calculatorService'];
+   
+    function controller($scope, calculatorService) {
+
+        $scope.user = {};
+        $scope.training = [];
+        $scope.checkTraining = function (user) {
+            $scope.training = calculatorService.checkTraining(user);
+        };
+
+
+    };
+
+})(angular);
+
