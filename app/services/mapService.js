bb5d4576c8eeb90a4dafd864b17c095ac40aa95d
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2016-10-21 21:02:47 +0300

    refactored a bit more

--- app/services/mapService.js
+++ app/services/mapService.js
@@ -1,7 +1,9 @@
 (function (angular) {
     'use strict';
 
-    var data = [];
+    var data = [],
+        urlLink = 'http://localhost:4816/api/maps';
+        
     angular
         .module('app')
         .factory('mapService', service);
@@ -20,13 +22,12 @@
         };
 
         function getMapData() {
-            $http.get('http://localhost:4816/api/maps', {
+            $http.get(urlLink, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
                 .then(function success(response) {
-
                     response.data.forEach(function (addressObject) {
                         data.push(addressObject);
                     });
@@ -34,7 +35,7 @@
                     console.log(data);
                 },
                 function error(response) {
-                    // console.log(response.statusText);
+                    console.log(response.statusText);
                 });
         }
 
@@ -42,9 +43,6 @@
         function addNewLocation(lastAddressPicked, lastNameGiven, lastDescriptionGiven) {
 
             var newAddressAddedData = {
-                // ID needs to be a string for marker to work properly.
-                // Why not use the name of the gym, nothing better than that, right??
-                // TODO: When enough time, drop the name altogether and use the id only in the HTML too.
                 id: lastNameGiven.toLowerCase(),
                 name: lastNameGiven,
                 address: lastAddressPicked,
@@ -61,17 +59,15 @@
             data.push(newAddressAddedData);
 
             // Update DB.
-            $http.post('http://localhost:4816/api/maps', JSON.stringify(newAddressAddedData), config)
+            $http.post(urlLink, JSON.stringify(newAddressAddedData), config)
                 .then(function success(response) {
                     // console.log(response.data)
                     console.log('Adding new address in DB succeeded!')
-
                 },
                 function error(response) {
                     console.log('Adding new address in DB failed!')
 
                 });
-
         }
 
         function deleteLocations() {
@@ -81,19 +77,16 @@
 
             //Update DB.
 
-            $http.delete('http://localhost:4816/api/maps/')
+            $http.delete(urlLink)
                 .then(function success(response) {
                     // console.log(response.data)
                     console.log('All addresses deletion in DB succeeded!')
-
                 },
                 function error(response) {
                     console.log('All addresses deletion in DB failed!')
-
                 });
 
             return data;
-
         }
 
         function deleteCertainLocation(nameToDelete) {
@@ -101,7 +94,6 @@
             // Update UI.
             data.forEach(function (iteratedAddress) {
                 if (nameToDelete.toLowerCase() === iteratedAddress.name.toLowerCase()) {
-
                     var index = data.indexOf(iteratedAddress);
                     if (index > -1) {
                         data.splice(index, 1);
@@ -112,11 +104,10 @@
             });
 
             // Update database.
-            $http.delete('http://localhost:4816/api/maps/' + nameToDelete.toLowerCase())
+            $http.delete(urlLink + '/' + nameToDelete.toLowerCase())
                 .then(function success(response) {
                     // console.log(response.data)
                     console.log('Deleting a certain address in DB succeeded!')
-
                 },
                 function error(response) {
                     // console.log(response.statusText);
@@ -129,5 +120,4 @@
             return ['Add Gym Location', 'Remove Certain Gym Location', 'Remove All Gym Locations', 'Hide All Gym Locations', 'Show All Gym Locations'];
         }
     }
-
 } (angular));
